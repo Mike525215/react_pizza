ad68d96fa62523c129552ac03e964388c19f327c
Миша Попов <misha@air-misa.home>
2023-07-19 00:33:07 -0400

    Changed some things in components. Also made owr app adaprive

--- frontend/pizza_app/src/components/Categories/Categories.js
+++ frontend/pizza_app/src/components/Categories/Categories.js
@@ -5,11 +5,12 @@ const Categories = () => {
         <div className={s.wrapper}>
             <div className={s.categories}>
                 <ul className={s.categoryBlock}>
-                    <li className={s.category}>Test</li>
-                    <li className={s.category}>Testtest</li>
-                    <li className={s.category}>Testtest</li>
-                    <li className={s.category}>Test</li>
-                    <li className={s.category}>Test</li>
+                    <li className={s.category}>Все</li>
+                    <li className={s.category}>Мясные</li>
+                    <li className={s.category}>Вегетерианские</li>
+                    <li className={s.category}>Гриль</li>
+                    <li className={s.category}>Острые</li>
+                    <li className={s.category}>Закрытые</li>
                 </ul>
             </div>
             <div className={s.sortBlock}>
