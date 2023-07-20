af4429a8a6e4a0f323b79c80ae105a3341f328fc
Миша Попов <misha@air-misa.home>
2023-07-20 02:52:14 -0400

    PostgreSQL was initialized

--- backend/pizza/pizza/settings.py
+++ backend/pizza/pizza/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'pizza.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'PASSWORD': 'OEh52tuQ4szFAxyizFVa',
+        'HOST': 'containers-us-west-171.railway.app',
+        'PORT': 6805,
+        'USER': 'postgres'
+
+
     }
 }
 
