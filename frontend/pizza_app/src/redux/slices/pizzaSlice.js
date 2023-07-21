b2f45a4cc23d59065bd90b2e28ed6dd457cb9362
Миша Попов <misha@air-misa.home>
2023-07-21 03:27:03 -0400

    Added new state filteredArray

--- frontend/pizza_app/src/redux/slices/pizzaSlice.js
+++ frontend/pizza_app/src/redux/slices/pizzaSlice.js
@@ -3,15 +3,19 @@ import {createSlice} from '@reduxjs/toolkit';
 export const pizzaSlice = createSlice({
     name: 'pizzaList',
     initialState: {
-        pizzaArray: []
+        pizzaArray: [],
+        filteredArray: []
     },
     reducers: {
         setArray: (state, action) => {
             state.pizzaArray = action.payload
+        },
+        setFilteredArray: (state, action) => {
+            state.filteredArray = action.payload
         }
     }
 });
 
-export const {setArray} = pizzaSlice.actions;
+export const {setArray, setFilteredArray} = pizzaSlice.actions;
 
 export default pizzaSlice.reducer;
