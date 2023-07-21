23fb1bb2da14d4276924877b90ee514a0b1c89cd
Миша Попов <misha@air-misa.home>
2023-07-21 02:20:08 -0400

    created new slice pizzaSlice

---
+++ frontend/pizza_app/src/redux/slices/pizzaSlice.js
@@ -0,0 +1,17 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+export const pizzaSlice = createSlice({
+    name: 'pizzaList',
+    initialState: {
+        pizzaArray: []
+    },
+    reducers: {
+        setArray: (state, action) {
+            state.pizzaArray = action.payload
+        }
+    }
+});
+
+export const {setArray} = pizzaSlice.actions;
+
+export default pizzaSlice.reducer;
