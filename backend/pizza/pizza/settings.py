2aba93ac8105795bb7fbbd0e875014d2ff2fede6
Миша Попов <misha@air-misa.home>
2023-07-21 03:12:06 -0400

    Changed postgreSQL on SQLite3. Returned all of pizzas and super user

--- backend/pizza/pizza/settings.py
+++ backend/pizza/pizza/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'pizza.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'PASSWORD': 'OEh52tuQ4szFAxyizFVa',
-        'HOST': 'containers-us-west-171.railway.app',
-        'PORT': 6805,
-        'USER': 'postgres'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
