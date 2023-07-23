8e3b23386c7e5291d342e204bc6ff557db299480
Миша Попов <misha@air-misa.home>
2023-07-23 00:11:41 -0400

    Provider with redux logic was relocated in routers.js from App.js

--- frontend/pizza_app/src/App.js
+++ frontend/pizza_app/src/App.js
@@ -2,18 +2,14 @@ import './App.css';
 import {Header} from './components/Header/Header';
 import {Categories} from './components/Categories/Categories';
 import {Items} from './components/Items/Items';
-import {Provider} from 'react-redux';
-import {store} from './redux/store';
 const App = () => {
 
     return (
-        <Provider store={store}>
-            <main>
-                <Header />
-                <Categories />
-                <Items />
-            </main>
-        </Provider>
+        <main>
+            <Header />
+            <Categories />
+            <Items />
+        </main>
     );
 };
 
--- frontend/pizza_app/src/routers/routers.js
+++ frontend/pizza_app/src/routers/routers.js
@@ -1,15 +1,18 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {App} from '../App';
 import {Cart} from '../components/Cart/Cart';
-
+import {Provider} from 'react-redux';
+import {store} from '../redux/store';
 const Routers = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<App />} path="" />
-                <Route element={<Cart />} path="/cart/" />
-            </Routes>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<App />} path="" />
+                    <Route element={<Cart />} path="/cart/" />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
     );
 };
 
