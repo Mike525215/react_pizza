3c59061c2137943999a1f868fd5c3a91103eb22d
Миша Попов <misha@air-misa.home>
2023-07-25 03:31:12 -0400

    created features setTotalSum(decr), setCount(decr) and deletePizza

--- frontend/pizza_app/src/redux/slices/cartSlice.js
+++ frontend/pizza_app/src/redux/slices/cartSlice.js
@@ -16,10 +16,19 @@ export const cartSlice = createSlice({
         },
         addPizza: (state, action) => {
             state.cartArray.push(action.payload);
+        },
+        deletePizza: (state, action) => {
+            state.cartArray.splice(action.payload, 1);
+        },
+        setTotalSumDecr: (state, action) => {
+            state.totalSum -= action.payload;
+        },
+        setCountDecr: (state, action) => {
+            state.count -= action.payload;
         }
     }
 
 });
 
-export const {setCount, setTotalSum, addPizza} = cartSlice.actions;
+export const {setCount, setTotalSum, addPizza, deletePizza, setTotalSumDecr, setCountDecr} = cartSlice.actions;
 export default cartSlice.reducer;
