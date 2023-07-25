768d1510c78db4fec087d0b7af4f84dbd401899e
Миша Попов <misha@air-misa.home>
2023-07-25 03:33:43 -0400

    included new features which changing states

--- frontend/pizza_app/src/components/Cart/CartItem/CartItem.js
+++ frontend/pizza_app/src/components/Cart/CartItem/CartItem.js
@@ -1,6 +1,9 @@
 import s from './CartItem.module.css';
+import {useDispatch, useSelector} from 'react-redux';
+import {deletePizza, setTotalSumDecr, setCountDecr} from '../../../redux/slices/cartSlice';
 
 const CartItem = (props) => {
+    const dispatcher = useDispatch();
     return (
         <div className={s.pizzaCard}>
             <img src={props.pizza.image} alt="pizza" className={s.cardImage} />
@@ -9,7 +12,11 @@ const CartItem = (props) => {
                 <span className={s.price}>Цена: {props.pizza.price} $</span>
                 <span className={s.weight}>Тесто: {props.pizza.weight}</span>
                 <span className={s.long}>Длинна: {props.pizza.long} см</span>
-                <button className={s.deleteBtn}>Удалить</button>
+                <button className={s.deleteBtn} onClick={() => {
+                    dispatcher(deletePizza(props.id));
+                    dispatcher(setTotalSumDecr(props.pizza.price));
+                    dispatcher(setCountDecr(1));
+                }}>Удалить</button>
             </div>
         </div>
     );
