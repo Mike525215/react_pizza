59c3273c4232f26058523b58f80ad3e964964b43
Миша Попов <misha@air-misa.home>
2023-07-25 04:00:37 -0400

    added state for changing pizza count in cart

--- frontend/pizza_app/src/components/Cart/CartItem/CartItem.js
+++ frontend/pizza_app/src/components/Cart/CartItem/CartItem.js
@@ -1,12 +1,20 @@
 import s from './CartItem.module.css';
-import {useDispatch, useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
+import {useState} from 'react';
 import {deletePizza, setTotalSumDecr, setCountDecr} from '../../../redux/slices/cartSlice';
 
 const CartItem = (props) => {
     const dispatcher = useDispatch();
+    const [totalCount, setCount] = useState(0);
+
     return (
         <div className={s.pizzaCard}>
             <img src={props.pizza.image} alt="pizza" className={s.cardImage} />
+            <div className={s.setCountBlock}>
+                    <button className={s.increase} onClick={() => setCount(totalCount + 1)}>+</button>
+                    <span className={s.pizzaAmount}>{totalCount}</span>
+                    <button className={s.decrease}  onClick={() => setCount(totalCount - 1)}>-</button>
+            </div>
             <div className={s.infoBlock}>
                 <span className={s.title}>{props.pizza.title}</span>
                 <span className={s.price}>Цена: {props.pizza.price} $</span>
