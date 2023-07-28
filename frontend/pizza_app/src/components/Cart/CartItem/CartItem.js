69edc7094a10786171ed5ccb2c2a319cb6e46003
Миша Попов <misha@air-misa.home>
2023-07-28 03:25:56 -0400

    Included new features in CartItem component

--- frontend/pizza_app/src/components/Cart/CartItem/CartItem.js
+++ frontend/pizza_app/src/components/Cart/CartItem/CartItem.js
@@ -1,10 +1,9 @@
 import s from './CartItem.module.css';
 import {useDispatch} from 'react-redux';
-import {deletePizza, setTotalAmountReduce, setTotalCountReduce} from '../../../redux/slices/cartSlice';
+import {deletePizza, setTotalAmountReduce, setTotalCountReduce, addPizza, reducePizzaCount, setTotalCount, setTotalAmount} from '../../../redux/slices/cartSlice';
 
 const CartItem = (props) => {
     const dispatcher = useDispatch();
-
     return (
         <div className={s.pizzaCard}>
             <img src={props.pizza.image} alt="pizza" className={s.cardImage} />
@@ -13,18 +12,25 @@ const CartItem = (props) => {
                 <span className={s.weight}>{props.pizza.weight}, {props.pizza.long} см</span>
             </div>
             <div className={s.setCountBlock}>
-                    <span className={s.decrease}>-</span>
+                    <span className={s.decrease} onClick={() => {
+                        dispatcher(reducePizzaCount(props.pizza));
+                        dispatcher(setTotalAmountReduce(props.pizza.price));
+                        dispatcher(setTotalCountReduce(1));
+                    }}>-</span>
                     <span className={s.pizzaCount}>{props.pizza.count}</span>
-                    <span className={s.increase}>+</span>
+                    <span className={s.increase} onClick={() => {
+                        dispatcher(addPizza(props.pizza));
+                        dispatcher(setTotalCount());
+                        dispatcher(setTotalAmount(props.pizza.price));
+                    }}>+</span>
             </div>
             <div className={s.priceBlock}>
                     <span className={s.price}>{props.pizza.price * props.pizza.count} $</span>
             </div>
-            <span className={s.deleteBtn} onClick={() => {
+            <span className={s.deleteBtn}><img onClick={() => {
                     dispatcher(deletePizza(props.id));
                     dispatcher(setTotalAmountReduce(props.pizza.price * props.pizza.count));
-                    dispatcher(setTotalCountReduce(props.pizza.count));
-                }}><img src="../delete.png" alt="delete" /></span>
+                    dispatcher(setTotalCountReduce(props.pizza.count))}} src="../delete.png" alt="delete" /></span>
         </div>
     );
 };
