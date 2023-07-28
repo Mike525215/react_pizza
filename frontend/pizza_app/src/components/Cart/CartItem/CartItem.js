8909cab49e18497acd5c5697e0c72553551552dd
Миша Попов <misha@air-misa.home>
2023-07-27 20:25:36 -0400

    Changed look of CartItem component in general

--- frontend/pizza_app/src/components/Cart/CartItem/CartItem.js
+++ frontend/pizza_app/src/components/Cart/CartItem/CartItem.js
@@ -8,22 +8,23 @@ const CartItem = (props) => {
     return (
         <div className={s.pizzaCard}>
             <img src={props.pizza.image} alt="pizza" className={s.cardImage} />
-            <div className={s.setCountBlock}>
-                    <button className={s.increase}>+</button>
-                    <span className={s.pizzaAmount}>{props.pizza.count}</span>
-                    <button className={s.decrease}>-</button>
-            </div>
             <div className={s.infoBlock}>
                 <span className={s.title}>{props.pizza.title}</span>
-                <span className={s.price}>Цена: {props.pizza.price} $</span>
-                <span className={s.weight}>Тесто: {props.pizza.weight}</span>
-                <span className={s.long}>Длинна: {props.pizza.long} см</span>
-                <button className={s.deleteBtn} onClick={() => {
+                <span className={s.weight}>{props.pizza.weight}, {props.pizza.long} см</span>
+            </div>
+            <div className={s.setCountBlock}>
+                    <span className={s.decrease}>-</span>
+                    <span className={s.pizzaCount}>{props.pizza.count}</span>
+                    <span className={s.increase}>+</span>
+            </div>
+            <div className={s.priceBlock}>
+                    <span className={s.price}>{props.pizza.price * props.pizza.count} $</span>
+            </div>
+            <span className={s.deleteBtn} onClick={() => {
                     dispatcher(deletePizza(props.id));
                     dispatcher(setTotalAmountReduce(props.pizza.price * props.pizza.count));
                     dispatcher(setTotalCountReduce(props.pizza.count));
-                }}>Удалить</button>
-            </div>
+                }}><img src="../delete.png" alt="delete" /></span>
         </div>
     );
 };
